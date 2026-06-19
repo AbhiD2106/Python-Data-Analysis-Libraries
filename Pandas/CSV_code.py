321c734c9dad4923e1a3980d31dfdcebe49608c0
Abhi <amodi2106@gmail.com>
2026-06-19 21:26:06 +0530

    Pandas_CSV_methods

--- Pandas/CSV_code.py
+++ Pandas/CSV_code.py
@@ -18,8 +18,8 @@ print(df)
 '''
 
 
-# df_bollywood = pd.read_csv(r'E:\my_work\Python_libs\Python-Data-Analysis-Libraries\Pandas\csv_rowfiles\bollywood.csv')
-# print(df_bollywood)
+df_bollywood = pd.read_csv(r'E:\my_work\Python_libs\Python-Data-Analysis-Libraries\Pandas\csv_rowfiles\bollywood.csv')
+print(df_bollywood)
 
 #head tail
 
@@ -30,3 +30,53 @@ print(subs_df)
 
 h = subs_df.head(5)
 print(h)
+
+#tail for previewing the last 5 rows of the dataframe
+
+t = subs_df.tail(5)
+print("\n ",t)
+
+
+#sample
+#use for randomly selecting rows from the dataframe
+
+s= df_bollywood.sample(3)
+print("\n ",s)
+
+#value_counts   
+#use for counting the occurrences of unique values in a column
+#count no of movies by actor
+
+vc = df_bollywood.value_counts()
+print("\n ",vc)
+
+
+#sort_values
+#use for sorting the dataframe based on a specific column
+
+
+df_vk = pd.read_csv(r'E:\my_work\Python_libs\Python-Data-Analysis-Libraries\Pandas\csv_rowfiles\kohli_ipl.csv',index_col='match_no')
+print(df_vk)
+
+sort = df_vk.sort_values("runs" , ascending=False).head(3)
+print("\n",sort)
+
+
+sort = df_vk.sort_values("runs")
+print("\n",sort)
+
+
+#sort values
+#original dataset change in asc order   
+
+si = df_vk.sort_values("runs" , inplace=True)
+print("\n",si)
+
+#inplace = true 
+#parmenent change in csv filess
+
+#sort index
+
+print("\n" , df_bollywood.sort_index(inplace=True))
+
+
